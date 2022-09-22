edfc5e6d2ef8964089b0c276c423f689b462b460
juicio8 <juicio8@gmail.com>
2022-09-22 05:41:11 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     length = len(sys.argv) - 1
     arg = "argument"
     print("{} argument{}{}".format(length,
-                                   " " if length == 1 else "s",
+                                   "" if length == 1 else "s",
                                    "." if length == 0 else ":"))
     for i in range(1, len(lists)):
         print("{}: {}".format(i, lists[i]))
