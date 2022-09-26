567dae89734c963cdbf2501e89b045b7368b607b
juicio8 <juicio8@gmail.com>
2022-09-26 01:48:20 -0700

    again

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max_item = -inf
+        ntive_inf = float('-inf')
+        max_item = ntive_inf
         for elem in my_list:
             if elem > max_item:
                 max_item = elem
         return max_item
     return None
+
+print(max_integer([-5, 7, 0, 8]))
