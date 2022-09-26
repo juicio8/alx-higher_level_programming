a220bfc80b21120e88bb4c04d52bdc1474334f11
juicio8 <juicio8@gmail.com>
2022-09-26 02:33:33 -0700

    initial commit for 11 and 12

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        if 0 <= idx <= my_list:
+        if 0 <= idx < len( my_list):
             for item in my_list:
                 if my_list.index(item) == idx:
                     my_list.remove(item)
