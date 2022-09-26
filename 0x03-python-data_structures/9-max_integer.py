647f6d9818b920b6102747f7d05a827dee66aa02
juicio8 <juicio8@gmail.com>
2022-09-26 01:35:35 -0700

    used -inf

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        max = mylist[0]
+        max = -inf
         for elem in my_list:
             if elem > max:
                 max = elem
