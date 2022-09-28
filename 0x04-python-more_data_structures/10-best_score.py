9e86b1bbc56488d26df77ada1dcd235d004a8563
juicio8 <juicio8@gmail.com>
2022-09-28 06:02:17 -0700

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_score = float('-inf')
+        max_key = ""
+        for key in a_dictionary:
+            if a_dictionary[key] > max_score:
+                max_score = a_dictionary[key]
+                max_key = key
+        return max_key
+    return None
