bcb8a500943e428cd63589694846037762121fc4
juicio8 <juicio8@gmail.com>
2022-09-30 01:42:01 -0700

    initial commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,6 @@ nt(roman_string):
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                  'M': 1000}
         string = list(roman_string)
-        print(string)
         sum = 0
         for i in string:
             if string.index(i) < len(string)-1:
