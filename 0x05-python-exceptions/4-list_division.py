562cb22cb7a141b5a5d17037d50d7f4c08c6b907
juicio8 <juicio8@gmail.com>
2022-10-10 06:47:28 -0700

    initial commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,6 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except ZeroDivisionError:
             print("division by 0")
+            result = 0
         except IndexError:
             print("out of range")
             result = 0
