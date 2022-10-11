2de589fcbbfb5a564c40681ff36daa0f72af9c27
juicio8 <juicio8@gmail.com>
2022-10-11 08:34:31 -0700

    initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines a class Square """
+
+
+class Square:
+    """Definition of Square
+
+    Attributes:
+        size (int): size of a square
+
+        """
+    def __init__(self, size):
+        """creating an instance of Square
+
+        Args:
+            size(int): size of a square          """
+        self.__size = size
