56f29032f97de5b0acdce3a8a5d86bbe8870d6aa
juicio8 <juicio8@gmail.com>
2022-10-11 12:40:03 -0700

    initial commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Defining a class Square"""
+
+
+class Square:
+    """ A class square
+    Attributes:
+        __size (int): size of the square
+    """
+    def __init__(self, size):
+        """ Initialize an instance of Square
+        Args:
+            size (int): size of the square"""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        else if size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method Area
+        Returns:
+            area of the square"""
+        return self.__size * self.__size
