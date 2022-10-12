3354af6833d43b654fed866644ccc21533711b1f
juicio8 <juicio8@gmail.com>
2022-10-12 04:28:30 -0700

    initial commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -41,7 +41,7 @@ class Square:
             value (int): size of a square """
         if value is not int:
             raise TypeError("size musbe be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
