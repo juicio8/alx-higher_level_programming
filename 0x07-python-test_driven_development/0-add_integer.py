24d3f391fbfa7e3f7f3d6fc3de3a92fb605b18d3
juicio8 <juicio8@gmail.com>
2022-10-17 04:10:24 -0700

    initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" add_integer module """
+
+
+def add_integer(a, b=98):
+    """ add_integer : a function to add two integers
+
+        Args:
+            a (int/float): the first integer
+            b (int/float): the second integer
+
+        Raises:
+            TypeError: when passing non integer argument
+
+        Returns:
+            (int) : sum of a and b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
