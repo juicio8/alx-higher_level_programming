c8ca1b412a64e51a367782813f80e680cc41669e
juicio8 <juicio8@gmail.com>
2022-10-24 13:26:20 -0700

    initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" The BaseGeometry module """
+
+
+class BaseGeometry:
+    """ BaseGeometry class"""
+    def area(self):
+        """ public instance method """
+        raise Exception("area() is not implemented")
