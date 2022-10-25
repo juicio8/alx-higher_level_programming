40241dd567b189ff2bd7216981f559b5e8ade596
juicio8 <juicio8@gmail.com>
2022-10-25 12:38:35 -0700

    initial commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ open and reads a file(UTF-8), prints to stdout """
     with open(filename, "r", encoding="UTF-8") as f:
-        readfile = f.read
+        readfile = f.read()
         print(readfile, end="")
