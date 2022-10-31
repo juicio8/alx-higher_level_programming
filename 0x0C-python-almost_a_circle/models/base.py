2eed236eaf25ccc8ab6c90f9f95b32993ee91f5d
juicio8 <juicio8@gmail.com>
2022-10-31 12:06:31 -0700

    fixed pep8 errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ The base.py module """
 
+
 class Base:
     """ Attributes:
         __nb_objects (int)
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """ initialize
         Args:
-            id (int): 
+            id (int):
         """
         if id:
             self.id = id
