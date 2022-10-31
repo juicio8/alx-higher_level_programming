3ef61d72f38fa759d13c007fbcfe77a0691a1daf
juicio8 <juicio8@gmail.com>
2022-10-31 02:30:11 -0700

    initial commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,6 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print('\n' * self.__position[1], end='')
             for i in range(self.__size):
                 print(" " * self.position[0], end='')
                 print("#" * self.__size)
