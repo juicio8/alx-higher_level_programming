bd7218c42fae620dddd5c9f85146eb8ec5850445
juicio8 <juicio8@gmail.com>
2022-10-31 13:32:01 -0700

    fixed raises errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,10 +30,14 @@ class Rectangle(Base):
             self.__height = height
         if x < 0:
             raise ValueError("x must be >= 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
         else:
             self.__x = x
         if y < 0:
             raise ValueError("y must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
         else:
             self.__y = y
 
