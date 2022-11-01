d5e79429e0d5a38e2110c9d26e22b56a44f2bc94
juicio8 <juicio8@gmail.com>
2022-11-01 04:42:29 -0700

    fixed error in square__str__

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" The Square module"""
+from models.rectangle import *
+from models.base import *
+
+
+class Square(Rectangle):
+    """ the Square class
+        Args:
+            size (int) : size of a square
+            x (int)
+            y(int)
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializing"""
+        self.size = size
+        super().__init__(size, size, x, y, id=None)
+
+    def __str__(self):
+        return '[Square] ({}) {}/{} - {}'.format(
+                self.id,
+                self.x,
+                self.y,
+                self.size)
