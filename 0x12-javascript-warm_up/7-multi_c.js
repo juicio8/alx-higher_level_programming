e9e09934baa0a15acb05156104d40f7bda5ae5e6
juicio8 <wisdomochiche@gmail.com>
2022-12-21 11:03:58 -0800

    initial commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const number = Number(process.argv[2])
+if (number) {
+  for (let i = 0; i < number; i++)
+  console.log('C is fun');
+} else {
+  console.log('Missing number of occurrences');
+}
