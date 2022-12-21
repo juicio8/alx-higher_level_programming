0708f3b2c53dd1e5d6a8e48ffc75de024d250fa5
juicio8 <wisdomochiche@gmail.com>
2022-12-21 10:53:24 -0800

    initial commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const sentences = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < sentences.length; i++) {
   console.log(sentences[i]);
 }
