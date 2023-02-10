fde74a3e55aa79ba92ebb20a96b0fe98a6f569c4
juicio8 <wisdomochiche@gmail.com>
2023-02-10 02:02:37 -0800

    initial commit

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 1. Root user
+--  a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL *.* TO 'user_0d_1'@'localhost';
