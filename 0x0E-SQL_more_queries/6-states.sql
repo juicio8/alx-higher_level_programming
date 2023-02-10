2ebcf82498af26a4168407720b8b08a40decabad
juicio8 <wisdomochiche@gmail.com>
2023-02-10 06:08:09 -0800

    second commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- 6. States table
+-- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	`id` INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	`name` VAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- 9. Cities by States
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
+FROM `cities`
+INNER JOIN `states`
+ON `cities`.`state_id` = `states`.`id`
+ORDER BY `cities`.`id` ASC;
