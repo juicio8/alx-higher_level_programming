f663fd0a162385378441caf34c002396ff73b1fb
Deni Ochiche <wisdomochiche@gmail.com>
2023-03-08 08:11:11 +0100

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # Lists all City objects from the database hbtn_0e_101_usa.
-# Usage: ./102-relationship_cities_states_list.py <mysql username> /
+""" Usage: ./102-relationship_cities_states_list.py <mysql username> /
 #                                                 <mysql password> /
-#                                                 <database name>
+                                                 <database name>
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
