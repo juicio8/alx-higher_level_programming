0957f98beafd0128fec68152366dae2ced68459f
Deni Ochiche <wisdomochiche@gmail.com>
2023-03-08 07:54:34 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,3 +21,4 @@ if __name__ == "__main__":
                               .filter(City.state_id == State.id) \
                               .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
