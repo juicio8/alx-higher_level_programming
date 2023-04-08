232c4da8672ab4806891e989999712f6ccf8e0ad
juicio8 <wisdomochiche@gmail.com>
2023-04-08 10:18:02 -0700

    fix: error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t - type: {}".format(type(data)))
         print("\t - content: {}".format(data))
-        print("\t - utf-8 content: {}".format(the_page.decode("utf-8")))
+        print("\t - utf-8 content: {}".format(data.decode("utf-8")))
