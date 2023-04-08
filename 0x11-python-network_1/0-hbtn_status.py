20dd6a1655db9016420d9bcfc1bf10aa144de660
juicio8 <wisdomochiche@gmail.com>
2023-04-08 09:59:25 -0700

    chmod +x 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 import urllib.request
 
 if __name__ = "__main__":
-  req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-  with urllib.request.urlopen(req) as res:
-    data = res.read()
-    print("Body response:")
-    print("\t - type: {}".format(type(data)))
-    print("\t - content: {}".format(data))
-    print("\t - utf-8 content: {}".format(the_page.decode("utf-8")))
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as res:
+        data = res.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(data)))
+        print("\t - content: {}".format(data))
+        print("\t - utf-8 content: {}".format(the_page.decode("utf-8")))
