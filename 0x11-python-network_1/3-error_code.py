bbc507050aebb1176cd5ecafd2d5281b0df9b11d
juicio8 <wisdomochiche@gmail.com>
2023-04-08 12:35:51 -0700

    fix: error in printing

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("ascii"))
     except urllib.error.HTTPError as e:
-        print("Error code: ".format(e.code))
+        print("Error code: {}".format(e.code))
