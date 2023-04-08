1a48ddb02f7e72e3817c55c77c8deffc0036a50c
Deni Ochiche <wisdomochiche@gmail.com>
2023-04-08 18:02:17 +0100

    documented 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import urllib.request
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 
 if __name__ = "__main__":
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
