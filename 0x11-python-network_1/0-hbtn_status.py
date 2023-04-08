8ef397daa4456b68bc3b04e6e4f9c36c51e1cbe8
Deni Ochiche <wisdomochiche@gmail.com>
2023-04-08 18:10:16 +0100

    fix: documentation 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status"""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
+
 
 if __name__ = "__main__":
     req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
